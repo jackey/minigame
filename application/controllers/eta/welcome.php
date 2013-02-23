d824208592dfb8a2f7a348cdf3223e82fdfc9c91
jackey <jziwenchen@gmail.com>
2013-02-24 01:34:21 +0800

    add 3 sub folders

---
+++ application/controllers/eta/welcome.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+
+		$this->load->library('session');
+		$this->load->helper('form');
+		$this->load->helper('url');
+		$this->load->library('form_validation');
+	}
+
+	public function index() {
+		$this->load->view('welcome_message');
+	}
+
+	//access: xxx.local/eta/welcome/hello
+	public function hello() {
+		print 'hello';
+	}
+}
